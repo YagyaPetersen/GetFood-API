f461c9906fd9578b2f0228714fe9f0929d7b6fae
Yagya Petersen <yagya@angleorange.co.za>
2018-11-29 16:53:05 +0200

    Updated

--- GetFood_API/Controllers/GetFoodController.cs
+++ GetFood_API/Controllers/GetFoodController.cs
@@ -1,41 +1,57 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Configuration;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Net.Mime;
 using System.Web.Http;
+using GetFood_API.Classes;
+using GetFood_API.Models.Response;
 
 namespace GetFood_API.Controllers
 {
     public class GetFoodController : ApiController
     {
-       GetFoodContext GetFoodContext = new GetFoodContext();
+        GetFoodContext GetFoodContext = new GetFoodContext();
 
-        [Route("api/customer")]
+        [Route("api/restaurant/{id}/food")]
         [HttpGet]
-        public IHttpActionResult GetAll(/*int customerId, int driverId*/)
+        public IHttpActionResult GetAll(int id)
         {
-            using (var newContext = new GetFoodContext())
-            {
+           
 
-                var all = GetFoodContext.Order.Include(a=>a.Customer).Include(a=>a.Driver)/*.Include(a=>a.Food).Include(a=>a.Orders).Include(a=>a.Orders.Customer).Include(a=>a.Orders.Driver).Include(a=>a.Food.Restaurant)*/
-                    //.Where(a=>a.RestaurantId == customerId && a.FoodId == driverId )
+                var all = GetFoodContext.Foods.Include(a => a.Restaurant)
+                    .Where(a => a.RestaurantId == id)
                     .ToList();
 
                 return Ok(all);
-            }
+            
         }
 
+        [Route("api/order")]
+        [HttpPost]
+        public IHttpActionResult PostOrder([FromBody]Customer CustomerInfo)
+        {
+           Customer customer = new Customer();
+            customer.FirstName = CustomerInfo.FirstName;
+            customer.LastName = CustomerInfo.LastName;
+            var a = GetFoodContext.Customers.Add(customer);
+            return Json(a);
+            
+        }
 
+        [Route("api/customers")]
+        [HttpGet]
+        public IHttpActionResult GetCus()
+        {
+                var all = GetFoodContext.Customers
+                    //.Where(a => a.RestaurantId == id)
+                    .ToList();
 
-
-
-
-
-
-
-
-
+                return Ok(all);
+            
+        }
     }
 }
---
+++ GetFood_API/Models/Response/CustomerDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GetFood_API.Models.Response
+{
+    public class CustomerDto
+    {
+        
+        public int CustomerId { get; set; }
+        
+        public string FirstName { get; set; }
+        
+        public string LastName { get; set; }
+
+
+    }
+}
