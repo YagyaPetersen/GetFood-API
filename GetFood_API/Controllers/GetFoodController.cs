c729094fbe9f84ab5471409a9a4e3f51d3d51fcf
Yagya Petersen <yagya@angleorange.co.za>
2018-12-05 15:56:11 +0200

    Updated

--- GetFood_API/Controllers/GetFoodController.cs
+++ GetFood_API/Controllers/GetFoodController.cs
@@ -24,53 +24,69 @@ namespace GetFood_API.Controllers
             return Ok(all);
         }
 
+
         [System.Web.Http.Route("api/order")]
         [System.Web.Http.HttpPost]
-        public IHttpActionResult PostOrder([FromBody]FoodOrder orderInfo)
+        public IHttpActionResult PostOrder([FromBody] FoodOrder orderInfo)
         {
             FoodOrder foodOrders = new FoodOrder();
 
+//==============Food=========================================================
             var allFoods = GetFoodContext.Foods.Include(a => a.Restaurant).Where(a => a.FoodId == orderInfo.FoodId).ToList().FirstOrDefault();
             allFoods.FoodId = orderInfo.FoodId;
             foodOrders.FoodId = orderInfo.FoodId;
             foodOrders.Food = allFoods;
 
+//===============Order=======================================================
             Orders orders = new Orders();
-            //var orders = GetFoodContext.Order.Include(a => a.Customer).Where(a => a.OrderId == orderInfo.OrderId).ToList().FirstOrDefault();
+            var storedOrders = GetFoodContext.Order.Include(a => a.Customer).Where(a => a.OrderId == orderInfo.OrderId).ToList().FirstOrDefault();
             orders.OrderId = orderInfo.OrderId;
             foodOrders.Orders = orders;
             foodOrders.OrderId = orders.OrderId;
-            
-            //orders.Orders.CustomerAddress = orderInfo.Orders.CustomerAddress;
 
-            //if (orders.DriverAcceptance = true)
-            //{
-            //    orders.DriverId = orders.DriverId;
-            //    orders.Driver = orderInfo.Orders.Driver;
-            //}
-            //else 
-            //    {
-            //        orders.Driver = null;
-            //        orders.DriverId = 0;
-            //    }
+//==============Customer======================================================
+            var allCustomers = GetFoodContext.Customers.Where(a => a.CustomerId == orderInfo.Orders.CustomerId).ToList().FirstOrDefault();
+            orders.CustomerId = allCustomers.CustomerId;
+            orders.CustomerAddress = orderInfo.Orders.CustomerAddress;
+            orders.Customer = allCustomers;
 
-            //GetFoodContext.Customers.Add(customer);
+//==============Order Properties==============================================
+            foodOrders.Orders.DriverAcceptance = orderInfo.Orders.DriverAcceptance;
+            foodOrders.Orders.RestaurantAcceptance = orderInfo.Orders.RestaurantAcceptance;
+            foodOrders.Orders.OverallFee = allFoods.Price + foodOrders.Orders.DeliveryFee;
+
+            if (orders.RestaurantAcceptance == false || orders.DriverAcceptance == false)
+            {
+                foodOrders.Orders.OrderStatus = "Pending...";
+            }
+            else
+            {
+                foodOrders.Orders.OrderStatus = "Order Confirmed";
+            }
+
+            var drivers = GetFoodContext.Drivers
+
+//==============Saving=========================================================
+            GetFoodContext.FoodOrders.Add(foodOrders);
             //GetFoodContext.SaveChanges();
             return Json(foodOrders);
         }
+
+
+
 
         [System.Web.Http.Route("api/deleteOrder/{id}")]
         [System.Web.Http.HttpDelete]
         public IHttpActionResult DeleteOrder(int id)
         {
-            var all = GetFoodContext.Order
-                .Where(a => a.OrderId == id)
+            var all = GetFoodContext.FoodOrders
+                .Where(a => a.FoodOrderId == id)
                 .FirstOrDefault();
 
-            GetFoodContext.Order.Remove(all);
+            GetFoodContext.FoodOrders.Remove(all);
             GetFoodContext.SaveChanges();
 
-            return Json(GetFoodContext.Order);
+            return Json(GetFoodContext.FoodOrders);
         }
     }
 }
